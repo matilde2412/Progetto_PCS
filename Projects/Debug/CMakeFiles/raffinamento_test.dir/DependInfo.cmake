871ca22fc0bbb5108727fa0848fd239e78fcae1e
unknown <s282666@studenti.polito.it>
2023-05-17 10:25:14 +0200

    init

---
+++ Projects/Debug/CMakeFiles/raffinamento_test.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Progetto_PCS/PCS2023_Exercises/Projects/Raffinamento/main_test.cpp" "CMakeFiles/raffinamento_test.dir/main_test.cpp.obj" "gcc" "CMakeFiles/raffinamento_test.dir/main_test.cpp.obj.d"
+  "C:/Progetto_PCS/PCS2023_Exercises/Projects/Raffinamento/src/empty_class.cpp" "CMakeFiles/raffinamento_test.dir/src/empty_class.cpp.obj" "gcc" "CMakeFiles/raffinamento_test.dir/src/empty_class.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Projects/Debug/CMakeFiles/raffinamento_test.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raffinamento_test.dir/main_test.cpp.obj"
+  "CMakeFiles/raffinamento_test.dir/main_test.cpp.obj.d"
+  "CMakeFiles/raffinamento_test.dir/src/empty_class.cpp.obj"
+  "CMakeFiles/raffinamento_test.dir/src/empty_class.cpp.obj.d"
+  "libraffinamento_test.dll.a"
+  "raffinamento_test.exe"
+  "raffinamento_test.exe.manifest"
+  "raffinamento_test.pdb"
+  "raffinamento_test[1]_tests.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raffinamento_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
