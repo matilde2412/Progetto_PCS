c550cd8e7e659696c82bf4120fda64417d9c2cda
matilde2412 <133853939+matilde2412@users.noreply.github.com>
2023-06-30 15:55:04 +0200

    Update main_program.cpp

--- Projects/Raffinamento/main_program.cpp
+++ Projects/Raffinamento/main_program.cpp
@@ -1,10 +1,24 @@
 #include "empty_class.hpp"
+#include <chrono>
 
 int main()
 {
-  ProjectLibrary::Empty empty;
+    auto start = std::chrono::high_resolution_clock::now();
 
-  empty.Show();
+    vector<ProjectLibrary::Punto> vettorePunti;
+    vector<ProjectLibrary::Lato> vettoreLati;
+    vector<ProjectLibrary::Triangolo> vettoreTriangoli;
+    vector<pair<int, double>> vettoreIdArea;
 
-  return 0;
+    ProjectLibrary::ImportPunti("C:/PCS2023_Exercises/Projects/Raffinamento/Dataset/Test1/Cell0Ds.csv", vettorePunti);
+    ProjectLibrary::ImportLati("C:/PCS2023_Exercises/Projects/Raffinamento/Dataset/Test1/Cell1Ds.csv", vettoreLati);
+    ProjectLibrary::ImportTriangoli("C:/PCS2023_Exercises/Projects/Raffinamento/Dataset/Test1/Cell2Ds.csv", vettoreTriangoli, vettoreLati,vettorePunti,vettoreIdArea);
+    ProjectLibrary::Raffinamento(vettorePunti, vettoreLati, vettoreTriangoli, vettoreIdArea);
+    ProjectLibrary::ExportPunti(vettorePunti);
+    ProjectLibrary::ExportLati(vettoreLati,vettorePunti);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::cout << "Tempo impiegato: " << duration.count() << " secondi" << std::endl;
+    return 0;
 }
